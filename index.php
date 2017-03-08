604ceb1ecf6488d4b42e458a4b2ff1d4817a2f1c
Gregory <g.vandenende@live.nl>
2017-03-08 16:41:58 +0100

    a test script that will show the data of cannonballs in object format.

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-Hallo
+Hallo Gregory, <br>
+<br>
+Bij deze de informatie die je wilt hebben:<br>
+<br>
+<br>
+<br>
 
 <?php
+$base_url = "http://services.runescape.com/m=itemdb_oldschool";
+$item_url = "/api/catalogue/detail.json?item=2";
+
+
+$unparsed_JSON = file_get_contents($base_url . $item_url);
+$JSON =  json_decode($unparsed_JSON);
+
+echo "<br>";
+var_dump($JSON);
 ?>
