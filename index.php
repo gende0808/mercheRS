e0fe8f1cfdd84156ad64fab7c20fdee9aa44395f
Gregory <g.vandenende@Live.nl>
2017-03-10 04:23:19 +0100

    test code

--- index.php
+++ index.php
@@ -7,12 +7,24 @@ Bij deze de informatie die je wilt hebben:<br>
 
 <?php
 $base_url = "http://services.runescape.com/m=itemdb_oldschool";
-$item_url = "/api/catalogue/detail.json?item=2";
+$item_url = "/api/catalogue/detail.json?item=";
 
 
-$unparsed_JSON = file_get_contents($base_url . $item_url);
-$JSON =  json_decode($unparsed_JSON);
+for($a=0; $a<150; $a++){
 
-echo "<br>";
-var_dump($JSON);
+    echo "Curernt recursion number = ".$a."<br>";
+    $item_nr = $a;
+    //file_get_contents() is being surpressed while also getting checked for returning false.
+    $unparsed_JSON = @file_get_contents($base_url . $item_url . $a);
+    if($unparsed_JSON == false){
+        echo "het is false";
+    } else {
+        $JSON = json_decode($unparsed_JSON);
+        var_dump($JSON);
+    }
+    sleep(2);
+echo "<br><br><br>";
+echo "<br><br><br>";
+
+}
 ?>
